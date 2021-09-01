de249a951beedb3aa25601c77b9d8b8e52407a81
George Thomas <georgefsthomas@gmail.com>
2021-09-01 14:36:55 +0100

    Formatting - break long line

--- release.nix
+++ release.nix
@@ -2,7 +2,10 @@ let
   pkgsNix =
     let
       # 14/07/21
-      haskellNix = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/59e14e256ef8d1ec35e083bc209d355913c4e725.tar.gz) { };
+      haskellNix = import
+        (builtins.fetchTarball
+          https://github.com/input-output-hk/haskell.nix/archive/59e14e256ef8d1ec35e083bc209d355913c4e725.tar.gz)
+        { };
 
       nixpkgsSrc = haskellNix.sources.nixpkgs-2105;
       nixpkgsArgs = haskellNix.nixpkgsArgs;
